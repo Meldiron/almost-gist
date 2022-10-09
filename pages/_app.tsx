96972ae94f16f13b36a89f41e8ce24bbde563c68
Matej Baco <matejbacocom@gmail.com>
2022-10-09 21:42:45 +0200

    Fix readme png, add og tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,46 @@ import { Page, Grid, Link } from "@geist-ui/core";
 import { AccountProvider } from "../contexts/account";
 import { PageHeader } from "../components/Page/Header";
 import { ThemeProvider } from "../contexts/theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Head>
+        <title>Almost Gist</title>
+        <meta
+          name="description"
+          content="Platform to share your markdowns with anyone."
+        />
+
+        <meta property="og:url" content="https://almost-gist.matejbaco.eu/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Almost Gist" />
+        <meta
+          property="og:description"
+          content="Platform to share your markdowns with anyone."
+        />
+        <meta
+          property="og:image"
+          content="https://almost-gist.matejbaco.eu/cover.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="almost-gist.matejbaco.eu" />
+        <meta
+          property="twitter:url"
+          content="https://almost-gist.matejbaco.eu/"
+        />
+        <meta name="twitter:title" content="Almost Gist" />
+        <meta
+          name="twitter:description"
+          content="Platform to share your markdowns with anyone."
+        />
+        <meta
+          name="twitter:image"
+          content="https://almost-gist.matejbaco.eu/cover.png"
+        />
+      </Head>
       <AccountProvider>
         <Page
           style={{
