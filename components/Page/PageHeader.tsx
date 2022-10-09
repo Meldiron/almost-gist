e1928e4e7f2befed2b390319b321bd5c13e478fe
Matej Baco <matejbacocom@gmail.com>
2022-10-09 11:29:48 +0200

    Init commit + basic UI

---
+++ components/Page/PageHeader.tsx
@@ -0,0 +1,97 @@
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import { FC, useState } from "react";
+
+import { Grid, Image, Page, Text } from "@geist-ui/core";
+import { Book, Moon, Settings, Sun } from "@geist-ui/icons";
+import packageData from "package.json";
+
+import { formatDate } from "@/utils/date";
+
+const ButtonRound = dynamic(() => import("@/components/Button/ButtonRound"));
+const DrawerSettings = dynamic(
+  () => import("@/components/Drawer/DrawerSettings")
+);
+
+interface PageHeaderProps {
+  themeType?: string;
+  switchTheme?: VoidFunction;
+}
+
+const PageHeader: FC<PageHeaderProps> = ({
+  themeType = "dark",
+  switchTheme,
+}) => {
+  const router = useRouter();
+  const [isDrawerVisible, setIsDrawerVisible] = useState(false);
+
+  return (
+    <Page.Header center>
+      <Grid.Container alignItems="center" gap={1} justify="space-between">
+        <Grid sm>
+          <Grid.Container alignItems="center" gap={1}>
+            <Grid>
+              <Image
+                alt="Logo"
+                height="56px"
+                src="/telescope.svg"
+                width="56px"
+              />
+            </Grid>
+            <Grid>
+              <Text
+                b
+                span
+                font={1.25}
+                margin={0}
+                style={{ display: "block", cursor: "pointer" }}
+                onClick={() => {
+                  router.push("/");
+                }}
+              >
+                {packageData.displayName}
+              </Text>
+              <Text span type="secondary">
+                {formatDate(new Date())}
+              </Text>
+            </Grid>
+          </Grid.Container>
+        </Grid>
+        <Grid>
+          <Grid.Container alignItems="center" gap={1}>
+            <Grid>
+              <ButtonRound
+                auto
+                aria-label="Glossary"
+                icon={<Book />}
+                onClick={() => {
+                  router.push("/glossary");
+                }}
+              />
+            </Grid>
+            <Grid>
+              <ButtonRound
+                auto
+                aria-label="Theme"
+                icon={themeType === "dark" ? <Moon /> : <Sun />}
+                onClick={switchTheme}
+              />
+            </Grid>
+            <Grid>
+              <ButtonRound
+                auto
+                aria-label="Settings"
+                icon={<Settings />}
+                onClick={() => setIsDrawerVisible(true)}
+              />
+            </Grid>
+          </Grid.Container>
+          <DrawerSettings
+            isVisible={isDrawerVisible}
+            setIsVisible={setIsDrawerVisible}
+          />
+        </Grid>
+      </Grid.Container>
+    </Page.Header>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,90 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import {
+  GeistProvider,
+  CssBaseline,
+  Page,
+  Grid,
+  Text,
+  Image,
+  Button,
+  Link,
+} from "@geist-ui/core";
+import { useState } from "react";
+import { Github } from "@geist-ui/icons";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [themeType, setThemeType] = useState("dark");
+
+  const switchTheme = () => {
+    setThemeType((lastThemeType) =>
+      lastThemeType === "dark" ? "light" : "dark"
+    );
+  };
+
+  return (
+    <GeistProvider themeType={themeType}>
+      <CssBaseline />
+
+      <Page
+        style={{
+          paddingTop: "2rem",
+          marginBottom: "2rem",
+          maxWidth: "1000px",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Page.Header center>
+          <Grid.Container alignItems="center" gap={1} justify="space-between">
+            <Grid sm>
+              <Grid.Container alignItems="center" gap={1}>
+                <Grid>
+                  <Image
+                    alt="Logo"
+                    height="40px"
+                    src="/logo.svg"
+                    width="40px"
+                  />
+                </Grid>
+                <Grid>
+                  <Text h4 type="default" style={{ margin: "0" }}>
+                    Almost Gist
+                  </Text>
+                </Grid>
+              </Grid.Container>
+            </Grid>
+            <Grid>
+              <Grid.Container alignItems="center" gap={1}>
+                <Grid>
+                  <Button icon={<Github />} auto type="success">
+                    Login with GitHub
+                  </Button>
+                </Grid>
+              </Grid.Container>
+            </Grid>
+          </Grid.Container>
+        </Page.Header>
+        <Page.Content>
+          <Component {...pageProps} />
+        </Page.Content>
+        <Page.Footer>
+          <Grid.Container justify="center">
+            <Grid>
+              Made with 💙 and{" "}
+              <Link href="https://appwrite.io/" block>
+                Appwrite
+              </Link>{" "}
+              by{" "}
+              <Link href="https://github.com/meldiron" block>
+                Matej "Meldiron" Bačo
+              </Link>
+              .
+            </Grid>
+          </Grid.Container>
+        </Page.Footer>
+      </Page>
+    </GeistProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
